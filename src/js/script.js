345d003513ec2824359917e4eed1cb69cc709640
新飞 <lixinfei315@126.com>
2018-05-03 18:47:44 +0800

    no message

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+//隐藏body loading
+$(document).ready(function() {
+  $(function() {
+    $('.bodyLoad').hide();
+  });
+});
+var vm = new Vue({
+  el: '#app',
+  data: function() {
+    return {};
+  },
+});
